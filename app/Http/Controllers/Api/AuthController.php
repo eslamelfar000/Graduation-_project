f17b644168487423c9dc03d0232860cd2611cae0
shroukelsweesy <shroukelsweesy@gmail.com>
2023-02-12 20:50:35 +0200

    well

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,27 +5,28 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-   /**
+   /** 
      * Create a new AuthController instance.
      *
      * @return void
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
-    /**
+    /** 
      * Get a JWT via given credentials.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function login(Request $request){
-    	$validator = Validator::make($request->all(), [
-            'email' => 'required|email',
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|string|email',
             'password' => 'required|string|min:6',
         ]);
         if ($validator->fails()) {
@@ -36,42 +37,48 @@ class AuthController extends Controller
         }
         return $this->createNewToken($token);
     }
-    /**
+    /** 
      * Register a User.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|between:2,100',
-            'email' => 'required|string|email|max:100|unique:users',
-            'phone' => 'required|string|min:6',
-            'password' => 'required|string|confirmed|min:6',
+            'type'       => 'sometimes|required|integer|in:0,1', // allow customer or seller only
+            'first_name' => 'required|string|between:2,100',
+            'last_name'  => 'nullable|string|between:2,100',
+            'email'      => 'required|string|email|max:100|unique:users',
+            'phone'      => 'required|string|min:6',
+            'password'   => 'required|string|confirmed|min:6',
         ]);
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
         $user = User::create(array_merge(
-                    $validator->validated(),
-                    ['password' => bcrypt($request->password)]
-                ));
+            $validator->validated(),
+            ['password' => bcrypt($request->password)]
+        ));
+        $user->refresh();
         return response()->json([
             'message' => 'User successfully registered',
             'user' => $user
         ], 201);
     }
 
-    /**
+    /** 
      * Log the user out (Invalidate the token).
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function logout() {
+    public function logout()
+    {
         auth()->logout();
         return response()->json(['message' => 'User successfully signed out']);
     }
 
-    protected function createNewToken($token){
+    protected function createNewToken($token)
+    {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -5,54 +5,24 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Exception;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends BaseController
 {
-    public function redirect(string $provider): RedirectResponse
+    public function redirectToProvider($provider)
     {
-        if (! in_array($provider, ['facebook', 'gmail'])) {
-            return redirect()->route('login')
-                ->withErrors('An unexpected error occurred.');
-        }
-
         return Socialite::driver($provider)->redirect();
     }
 
-    public function callback(string $provider): RedirectResponse
+    public function handleProviderCallback($provider)
     {
-        if (! in_array($provider, ['facebook', 'gmail'])) {
-            return redirect()->route('login')
-                ->withErrors('An unexpected error occurred.');
-        }
+        $user = Socialite::driver($provider)->user();
+        $userId = $user->getEmail();
 
-        try {
-            $user = Socialite::driver($provider)->user();
-        } catch (Exception $e) {
-            return redirect()->route('login')
-                ->withErrors('An unexpected error occurred.');
-        }
-
-        if (! $user->getEmail()) {
-            return redirect()->route('login')
-                ->withErrors('The email address is invalid.');
-        }
-
-        if ($exists = User::where('email', $user->getEmail())->first()) {
-            if ($exists->oauth_provider !== $provider || (string) $exists->oauth_id !== (string) $user->getId()) {
-                return redirect()->route('login')
-                    ->withErrors('The email address is already associated with another account.');
-            }
-        }
 
         User::unguard();
-
 
         $resource = User::firstOrCreate(
             [
@@ -65,23 +35,13 @@ class SocialiteController extends BaseController
                 'oauth_id' => $user->getId(),
 
                 'nickname' => $user->getNickname(),
-                'name' => $user->getName(),
+                'first_name' => $user->getName(),
                 'email' => $user->getEmail(),
                 'avatar' => $user->getAvatar(),
 
                 'email_verified_at' => now(),
                 // 'remember_token' => Str::random(10),
                 'password' => bcrypt(Str::random(32)),
-
-                'extra_attributes' => [
-                    'oauth' => Crypt::encrypt([
-                        'user' => $user,
-
-                        'token' => $user->token ?? null,
-                        'refreshToken' => $user->refreshToken ?? null,
-                        'expiresIn' => $user->expiresIn ?? null,
-                    ]),
-                ],
             ],
         );
 
