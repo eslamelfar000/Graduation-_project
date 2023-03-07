12089e0a41aa8ce93cfa2b68a7f32f6cb7e27122
omnya azmy <116688348+OmnyaAzmy@users.noreply.github.com>
2023-03-07 13:48:27 +0200

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,32 +2,30 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ReviewController extends Controller
 {
-    public function store(Request $request ){
-        
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'product_id'    => 'required',
-            'review' => 'required',
-            'star'    => 'required',
-    
+            'product_id'    => ['required', 'integer', 'exists:products,id'],
+            'star'    => ['required', 'integer', 'min:1', 'max:5'],
+            'review' => ['required', 'string'],
         ]);
-        if ($validator->fails()) {
 
+        if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
-        $review= new Review;
-        $review->product_id = $request->product_id;
-        $review->review = $request->review;
-        $review->star = $request->star;
-        
-        $result= $review->save();
 
-        return response($review,200,["add successfully"]);
+        $review = new Review();
+        $review->fill($validator->validated());
+        $review->user_id = Auth::id();
+        $review->save();
+
+        return response($review, 200, ["added successfully"]);
     }
 }
