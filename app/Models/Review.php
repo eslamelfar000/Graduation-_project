4cf7d1915e684f57c1b2f37f1ac0a1cf481b001e
omnya azmy <116688348+OmnyaAzmy@users.noreply.github.com>
2023-03-07 13:49:52 +0200

    Update Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,20 +2,29 @@
 
 namespace App\Models;
 
-use App\Models\Shop;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    use HasFactory;
 
     protected $fillable = [
-        'review','star'  ,'product_id'
+        'review',
+        'star',
+        'product_id',
+        'user_id',
     ];
-    public function product() 
-{ 
-    return $this->belongsTo(Shop::class); 
-}
 
-    
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
