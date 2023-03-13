748fd9504912174928f370e8aec529d625751093
omnya azmy <116688348+OmnyaAzmy@users.noreply.github.com>
2023-03-13 22:28:06 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'is.admin' => \App\Http\Middleware\IsAdmin::class,
+        'is.seller' => \App\Http\Middleware\IsSeller::class
+
     ];
 }
