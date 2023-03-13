757c5ae8732add428d9d66f64bb48c81a86b4c6c
omnya azmy <116688348+OmnyaAzmy@users.noreply.github.com>
2023-03-13 22:32:09 +0200

    Update ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Resources\ReviewResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
@@ -22,34 +23,35 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'product' => [
-            'id'=>  $this->id,
+            'id' =>  $this->id,
             'title' => $this->title,
             'newPrice' => $this->newPrice,
             'oldPrice' => $this->oldPrice,
             'offer' => $this->offer,
             'reviews_count' => $this->reviews_count,
+            'rating' => $this->rating,
+            'is_complete' => $this->is_complete,
+            'is_special' => $this->is_special,
+
 
             'details' => [
-                #'category' => $this->category,
                 'abstract' => $this->abstract,
-                #'featuer' => $this->featuer,
                 'pin_code' => $this->pin_code,
                 'description' => $this->description,
-                'videos' => $this->videos,
+                'videos' => [$this->videos],
                 'video_html' => $this->when($this->videos, function () {
                     $query_str = parse_url($this->videos, PHP_URL_QUERY);
-    
+
                     if (! $query_str) {
                         return null;
                     }
-    
+
                     parse_str($query_str, $params);
-    
+
                     if (! isset($params['v'])) {
                         return null;
                     }
-    
+
                     return '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $params['v'] . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
                 }, null),
 
@@ -59,16 +61,15 @@ class ProductResource extends JsonResource
                         ->map(fn ($item) => ucfirst(trim($item)))
                         ->values();
                 }, null),
-                
-                 'categories' => $this->when($this->category, function () {
-                    return collect(explode(',', $this->category))
+
+                'featuers' => $this->when($this->featuer, function () {
+                    return collect(explode(',', $this->featuer))
                         ->filter(fn ($item) => trim($item))
                         ->map(fn ($item) => ucfirst(trim($item)))
                         ->values();
                 }, null),
-
-                'featuers' => $this->when($this->featuer, function () {
-                    return collect(explode(',', $this->featuer))
+                'category' => $this->when($this->category, function () {
+                    return collect(explode(',', $this->category))
                         ->filter(fn ($item) => trim($item))
                         ->map(fn ($item) => ucfirst(trim($item)))
                         ->values();
@@ -83,24 +84,25 @@ class ProductResource extends JsonResource
                     $images = [];
 
                     if ($this->hasMedia('image')) {
-                        $images[] = 
-                            $this?->getFirstMediaUrl('image');
+                        $images[] =[
                         
+                            $this?->getFirstMediaUrl('image')
+                        ];
                         for ($i = 2; $i <= 6; $i++) {
                             $media = $this?->getFirstMedia('images', ['form_key' => 'image_' . $i]);
-    
+
                             if (! $media) {
                                 continue;
                             }
                             $images[] = $media?->getUrl();
-                            
                         }
                     }
-                    
+
                     return $images;
                 }),
+                'reviews' => ReviewResource::collection($this->whenLoaded('reviews')),
             ],
-        ],
+
         ];
     }
 }
